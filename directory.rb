d22969e3e5e4df278dec8b78e75a7bb58fa9bc3f
Goncalo Gordo <goncalogordo@gmail.com>
2021-05-24 16:14:21 +0100

    filter list of students based on name length

--- directory.rb
+++ directory.rb
@@ -25,7 +25,8 @@ end
 def print(students)
   students.each_with_index do |student, index| 
     # puts "#{index + 1}. #{student[:name]} (#{student[:cohort]} cohort)"
-    puts "#{index + 1}. #{student[:name]} (#{student[:cohort]} cohort)" if student[:name].chr == 'D'
+    # puts "#{index + 1}. #{student[:name]} (#{student[:cohort]} cohort)" if student[:name].chr == 'D'
+    puts "#{index + 1}. #{student[:name]} (#{student[:cohort]} cohort)" if student[:name].length < 12
   end
 end
 
