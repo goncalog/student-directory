f40952f2dece6dd9c2e7e0c2b304691ba4dc815a
Goncalo Gordo <goncalogordo@gmail.com>
2021-05-25 14:02:32 +0100

    add save list feature

--- directory.rb
+++ directory.rb
@@ -45,6 +45,7 @@ end
 def print_menu
   puts "1. Input the students"
   puts "2. Show the students"
+  puts "3. Save the list to students.csv"
   puts "9. Exit" # 9 because we'll be adding more items
 end
 
@@ -54,6 +55,17 @@ def show_students(students)
     print(students)
     print_footer(students)
   end
+end
+
+def save_list(students)
+  # open the file for writing
+  file = File.open("students.csv", "w")
+  students.each do |student|
+    student_data = [student[:name], student[:cohort]]
+    csv_line = student_data.join(",")
+    file.puts csv_line
+  end
+  file.close
 end
 
 def interactive_menu
@@ -66,6 +78,8 @@ def interactive_menu
       students = input_students 
     when "2"
       show_students(students)
+    when "3"
+      save_list(students)
     when "9"
       exit # this will cause the program to terminate
     else
