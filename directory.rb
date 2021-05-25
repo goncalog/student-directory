95f9f9105a7c8b7a0e4119be33245fe427a74f86
Goncalo Gordo <goncalogordo@gmail.com>
2021-05-25 13:49:42 +0100

    refactor interactive_menu

--- directory.rb
+++ directory.rb
@@ -42,25 +42,30 @@ def print_footer(names)
   puts "Overall, we have #{names.count} great students"
 end
 
+def print_menu
+  puts "1. Input the students"
+  puts "2. Show the students"
+  puts "9. Exit" # 9 because we'll be adding more items
+end
+
+def show_students(students)
+  if !students.empty? 
+    print_header
+    print(students)
+    print_footer(students)
+  end
+end
+
 def interactive_menu
   students = []
   loop do
-    # 1. print the menu and ask the user what to do
-    puts "1. Input the students"
-    puts "2. Show the students"
-    puts "9. Exit" # 9 because we'll be adding more items
-    # 2. read the input and save it into a variable
+    print_menu()
     selection = gets.chomp
-    # 3. do what the user has asked
     case selection
     when "1"
       students = input_students 
     when "2"
-      if !students.empty? 
-        print_header
-        print(students)
-        print_footer(students)
-      end
+      show_students(students)
     when "9"
       exit # this will cause the program to terminate
     else
@@ -70,4 +75,4 @@ def interactive_menu
 end
 
 #nothing happens until we call the methods
-interactive_menu
+interactive_menu()
